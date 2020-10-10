db68e24fc3d6ac51c783ad4742a79f39ccd46d75
郭鑫铖 <7833460+Gxc162@user.noreply.gitee.com>
2020-10-10 18:04:27 +0800

    [第一次提交：对接接口]

---
+++ src/settings.js
@@ -0,0 +1,24 @@
+/*
+ * @Author: your name
+ * @Date: 2020-09-21 09:24:06
+ * @LastEditTime: 2020-10-08 09:15:48
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \briup02\vue\huang-template\src\settings.js
+ */
+module.exports = {
+
+  title: '叮叮打车',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
